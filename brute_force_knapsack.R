51310399424e7156ddf0f69944763d9c604f1689
unknown <Clara.Schartner@gmail.com>
2015-10-02 13:08:59 +0200

    working!

--- brute_force_knapsack.R
+++ brute_force_knapsack.R
@@ -1,5 +1,5 @@
 set.seed(42)
-n <- 20
+n <- 200
 knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 
 brute_force_knapsack <- function(x, W){
@@ -7,18 +7,20 @@ brute_force_knapsack <- function(x, W){
     stopifnot(x[1] >= 0)
     stopifnot(x[2] >= 0)
     
-    poss<-matrix(0,nrow=2^nrow(w),ncol=32)
-    for (i in 1:(2^nrow(w))){
+    poss<-matrix(0,nrow=2^nrow(x),ncol=32)
+    for (i in 1:(2^nrow(x))){
     poss[i,]<-as.numeric(intToBits(i))}
     temp<-NULL
     temp.value<-NULL
     ind<-NULL
     for( i in 1:nrow(poss)){
       if(sum(poss[i,]*x[1])<W){
-        ind[length(ind)]<-which(poss[i,]==1)
+        ind[length(ind)+1]<-i
    temp[length(temp)+1]<-sum(poss[i,]*x[1])
    temp.value[length(temp.value)+1]<-sum(poss[i,]*x[2])
       }
     }
     
+    numberofbin<-ind[which(temp.value==max(temp.value))[1]]
+   print( which(poss[numberofbin,]==1))
 }

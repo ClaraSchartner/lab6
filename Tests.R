3a6f72822717e6fa84a6d293d0f51a44c15c648b
unknown <Clara.Schartner@gmail.com>
2015-10-02 19:09:34 +0200

    some code for lineprof

--- Tests.R
+++ Tests.R
@@ -1,4 +1,5 @@
 n<-999999
 knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 test<-function(x)
-  {stopifnot(is.data.frame(x))}
+{stopifnot(is.data.frame(x))}
+#not working

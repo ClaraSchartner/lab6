3e0101794fdbf3eeee939ce8e633edc8246ec3db
unknown <Clara.Schartner@gmail.com>
2015-10-02 12:10:20 +0200

    starting

--- brute_force_knapsack.R
+++ brute_force_knapsack.R
@@ -1,11 +1,17 @@
 set.seed(42)
-n <- 2000
+n <- 20
 knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 
 brute_force_knapsack <- function(x, W){
     stopifnot(is.data.frame(x))
     stopifnot(x[1] >= 0)
     stopifnot(x[2] >= 0)
+    
+    poss<-matrix(0,nrow=nrow(w),ncol=32)
+    for (i in 1:2^nrow(w)){
+    poss[i,]<-as.numeric(intToBits(i))}
+    for( i in 1:nrow(poss))
+    poss[1,]*w[1]
     
     
 }

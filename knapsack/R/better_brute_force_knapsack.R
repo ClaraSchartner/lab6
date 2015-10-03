e9a229f77524cb6c6d0eb1317993d8be8dd365f5
unknown <Clara.Schartner@gmail.com>
2015-10-03 21:40:02 +0200

    corrected minor mistakes

--- knapsack/R/better_brute_force_knapsack.R
+++ knapsack/R/better_brute_force_knapsack.R
@@ -1,8 +1,8 @@
 set.seed(42)
 n <- 2000
-better_knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
+knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
-brute_force_knapsack <- function(x, W){
+better_brute_force_knapsack <- function(x, W){
 
   stopifnot(class(x)=="data.frame") 
 

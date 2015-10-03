9a3799060efaa836dfba6e18c6e7784e8e247a21
unknown <Clara.Schartner@gmail.com>
2015-10-03 21:40:46 +0200

    initial brute

--- knapsack/R/brute_force_knapsack.R
+++ knapsack/R/brute_force_knapsack.R
@@ -3,11 +3,11 @@ n <- 200
 knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 
 brute_force_knapsack <- function(x, W){
-    #stopifnot(is.data.frame(x))
-    stopifnot(class(x)=="data.frame") #is faster
-    #stopifnot(x[1] >= 0)
-    #stopifnot(x[2] >= 0)
-    stopifnot(all(x >= 0)) #a lot faster
+    stopifnot(is.data.frame(x))
+
+    stopifnot(x[1] >= 0)
+    stopifnot(x[2] >= 0)
+
     
     poss<-matrix(0,nrow=2^nrow(x),ncol=32)
     for (i in 1:(2^nrow(x))){
@@ -22,7 +22,7 @@ brute_force_knapsack <- function(x, W){
             temp.value[length(temp.value)+1]<-sum(poss[i,]*x[2])
         }
     }
-    
+
     numberofbin<-ind[which(temp.value==max(temp.value))[1]]
     print( which(poss[numberofbin,]==1))
     print( sum(poss[numberofbin,]*x[2]))

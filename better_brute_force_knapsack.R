4de5e60ec2b129139610a10cb961a0606c1bfb67
Araya Eamrurksiri <araea382@student.liu.se>
2015-10-04 09:05:47 +0200

    temporary move better brute force out of the package

---
+++ better_brute_force_knapsack.R
@@ -0,0 +1,26 @@
+set.seed(42)
+n <- 2000
+knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
+brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500)
+better_brute_force_knapsack <- function(x, W){
+
+  stopifnot(class(x)=="data.frame") 
+
+  stopifnot(all(x >= 0))
+  
+  poss<-matrix(0,nrow=2^nrow(x),ncol=32)
+  for (i in 1:(2^nrow(x))){
+    poss[i,]<-as.numeric(intToBits(i))}
+  temp<-NULL
+  temp.value<-NULL
+  ind<-NULL
+
+  temp<-apply(poss,1,"%*%",c(x[[1]],rep(0,ncol(poss)-length(x[[1]]))))
+  temp.value<-apply(poss,1,"%*%",c(x[[2]],rep(0,ncol(poss)-length(x[[2]]))))
+  select<-temp<W
+  temp<-select*temp
+  temp.value<-select*temp.value
+  numberofbin<-which(temp.value==max(temp.value))[1]
+  print( which(poss[numberofbin,]==1))
+  print( sum(poss[numberofbin,]*x[2]))
+}

5b45915b3a5ce33709e9786caa2805f39cc0f739
unknown <Clara.Schartner@gmail.com>
2015-10-05 13:22:04 +0200

    moving to include right functions in package

--- knapsack/R/old_brute_force_knapsack.R
+++ knapsack/R/old_brute_force_knapsack.R
@@ -20,29 +20,32 @@
 #n <- 200
 #knapsack_objects <-data.frame(w=sample(1:4000, size = n, replace = TRUE),v=runif(n = n, 0, 10000))
 
-brute_force_knapsack <- function(x, W){
+old_brute_force_knapsack <- function(x, W){
+    stopifnot(is.data.frame(x))
 
-  stopifnot(class(x)=="data.frame") 
+    stopifnot(x[1] >= 0)
+    stopifnot(x[2] >= 0)
 
-  stopifnot(all(x >= 0))
-  
-  poss<-matrix(0,nrow=2^nrow(x),ncol=32)
-  for (i in 1:(2^nrow(x))){
+    
+    poss<-matrix(0,nrow=2^nrow(x),ncol=32)
+    for (i in 1:(2^nrow(x))){
     poss[i,]<-as.numeric(intToBits(i))}
-  temp<-NULL
-  temp.value<-NULL
-  ind<-NULL
+    temp<-NULL
+    temp.value<-NULL
+    ind<-NULL
+    for( i in 1:nrow(poss)){
+        if(sum(poss[i,]*x[1])<W){
+            ind[length(ind)+1]<-i
+            temp[length(temp)+1]<-sum(poss[i,]*x[1])
+            temp.value[length(temp.value)+1]<-sum(poss[i,]*x[2])
+        }
+    }
 
-  temp<-apply(poss,1,"%*%",c(x[[1]],rep(0,ncol(poss)-length(x[[1]]))))
-  temp.value<-apply(poss,1,"%*%",c(x[[2]],rep(0,ncol(poss)-length(x[[2]]))))
-  select<-temp<W
-  temp<-select*temp
-  temp.value<-select*temp.value
-  numberofbin<-which(temp.value==max(temp.value))[1]
-  
-  value <- sum(poss[numberofbin,]*x[2])
-  elements <- which(poss[numberofbin,]==1)
-  
-  ls <- list("value"=round(value), "elements"=elements)
-  return(ls)
+    numberofbin<-ind[which(temp.value==max(temp.value))[1]]
+    
+    value <- sum(poss[numberofbin,]*x[2])
+    elements <- which(poss[numberofbin,]==1)
+    
+    ls <- list("value"=round(value), "elements"=elements)
+    return(ls)
 }

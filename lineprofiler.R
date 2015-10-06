d3b305d21aec4792c11e9950249ddaf48ef840e2
unknown <Clara.Schartner@gmail.com>
2015-10-06 12:01:24 +0200

    small changes, not running all of them now

--- lineprofiler.R
+++ lineprofiler.R
@@ -15,14 +15,19 @@ source("C:/Users/Clara/Desktop/Advanced R/lab5_API/lab5API/ggdirections/R/direct
 
 source("C:/Users/Clara/Desktop/Advanced R/lab5_API/lab6/knapsack/R/brute_force_knapsack.R")
 knap.lin<-lineprof(brute_force_knapsack(x = knapsack_objects[1:8,], W = 3500))
-knap.lin[c(1:50),]
+#knap.lin
 
 
 
 source("C:/Users/Clara/Desktop/Advanced R/lab5_API/lab6/knapsack/R/greedy_knapsack.R")
 knap.greedy.lin<-lineprof(greedy_knapsack1(x = knapsack_objects[1:8,], W = 3500))
-knap.greedy.lin[c(1:50),]
+#knap.greedy.lin
 #not working
 source("C:/Users/Clara/Desktop/Advanced R/lab5_API/lab6/knapsack/R/knapsack_dynamic.R")
 knap.dyn.lin<-lineprof(knapsack_dynamic(x = knapsack_objects[1:8,], W = 3500))
-knap.dyn.lin[c(1:50),]
+#knap.dyn.lin
+
+
+source("C:/Users/Clara/Desktop/Advanced R/lab5_API/lab6/testforline.R")
+knap.lin<-lineprof(test(x = knapsack_objects[1:8,], W = 3500))
+#knap.lin
